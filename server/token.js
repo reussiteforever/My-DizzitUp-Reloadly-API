ec0355d6e86f66aad8a81410e57290302554b3db
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-11-07 12:55:14 +0000

    first access token

---
+++ server/token.js
@@ -0,0 +1,36 @@
+const ReloadlyAuthentication = require("@reloadly/reloadly.authentication");
+const ReloadlyCore = require("@reloadly/reloadly.core");
+const express = require("express");
+
+let app = express();
+app.use(express.json());
+
+app.get("/token", async (req, res) => {
+    let api = new ReloadlyAuthentication.AuthenticationApi(
+    "aLZwVjy3BTrx8yaFsb4iKgqFE2VGkpfs",
+    "jOzKRlZVfB-WNlS88TEVt9uiNZtubg-XlKcY8D0cOWiZa6OIjcPe2BI7ZLAZ2an",
+    ReloadlyCore.ServiceURLs.AIRTIME_SANDBOX
+    );
+    let operation = api.clientCredentials();
+    let request = operation.getAccessToken();
+    let response = await request.execute();
+    console.log("access token: " + response);
+    res.send(`<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simple To-Do App</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+        <h1 class="display-4 text-center py-1">Top-up App</h1>
+        <p class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+        We've received your access token
+        </p>
+        </div>
+    </body>
+    </html>`);
+});
+
+app.listen(5000);
